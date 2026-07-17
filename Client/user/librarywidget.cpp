598126d1af903e46135a9b875ea1c71e0182d46d
Onyx1907 <f.abbasi2804@gmail.com>
2026-07-17 17:57:52 +0330

    feat(library): add shelves UI, network and logic of request shelves and their books

--- Client/user/librarywidget.cpp
+++ Client/user/librarywidget.cpp
@@ -6,9 +6,9 @@
 #include <QScrollBar>
 #include "clientnetworkmanager.h"
 #include "bookcard.h"
-#include "book.h"
 #include "constants.h"
 #include "bookinfocard.h"
+#include <QMenu>
 
 LibraryWidget::LibraryWidget(int userID, QWidget *parent)
     : QWidget(parent), m_userID(userID)
@@ -21,6 +21,8 @@ LibraryWidget::LibraryWidget(int userID, QWidget *parent)
 
     connect(&ClientNetworkManager::instance(), &ClientNetworkManager::responseReceived,
             this, &LibraryWidget::processNetworkData);
+
+    connect(ui->shelves_listWidget, &QListWidget::itemClicked, this, &LibraryWidget::on_shelf_clicked);
 
 
     ui->purchased_listWidget->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
@@ -44,6 +46,7 @@ LibraryWidget::~LibraryWidget()
 void LibraryWidget::showEvent(QShowEvent *event) {
     QWidget::showEvent(event);
 
+    requestShelves();
     int currentIndex = ui->tabWidget->currentIndex();
     on_tabWidget_currentChanged(currentIndex);
 
@@ -159,13 +162,36 @@ void LibraryWidget::requestShelves(){
 }
 
 
+void LibraryWidget::requestShelfBooks(int shelfID){
+    if(ClientNetworkManager::instance().connectToServer()){
+
+        QJsonObject data;
+        data["shelf_id"] = shelfID;
+
+        ClientNetworkManager::instance().sendRequest("GET_SHELF_BOOKS", data, true);
+    }
+    else{
+        ui->error_label->setText("خطا در برقراری اتصال");
+        QTimer::singleShot(3000, this, [this](){
+            ui->error_label->setText("");
+        });
+    }
+}
+
+
 void LibraryWidget::processNetworkData(const QString& action, const QJsonObject& data){
-    // if (action == "GET_PURCHASED_BOOKS_RESPONSE"){
-    //     handleGetPurchasedBooks(data);
-    // }
-    // else if(action == "GET_SAVED_BOOKS_RESPONSE"){
-    //     handleGetSavedBooks(data);
-    // }
+    if (action == "GET_PURCHASED_BOOKS_RESPONSE"){
+        handleGetPurchasedBooks(data);
+    }
+    else if(action == "GET_SAVED_BOOKS_RESPONSE"){
+        handleGetSavedBooks(data);
+    }
+    else if(action == "GET_SHELVES_RESPONSE"){
+        handleGetShelves(data);
+    }
+    else if(action == "GET_SHELF_BOOKS_RESPONSE"){
+        handleGetShelfBooks(data);
+    }
     //موقت
 }
 
@@ -189,6 +215,8 @@ void LibraryWidget::handleGetPurchasedBooks(const QJsonObject& response) {
             0.0);
 
         BookCard* card = new BookCard(book, this);
+
+        connect(card, &BookCard::clicked, this, &LibraryWidget::on_bookCard_clicked);
 
         QListWidgetItem* item = new QListWidgetItem(ui->purchased_listWidget);
         item->setSizeHint(card->sizeHint());
@@ -223,5 +251,144 @@ void LibraryWidget::handleGetSavedBooks(const QJsonObject& response) {
         ui->saved_listWidget->setItemWidget(item, card);
     }
 }
+
+void LibraryWidget::handleGetShelves(const QJsonObject& response) {
+    ui->shelves_listWidget->clear();
+    m_shelves.clear();
+
+    if (response["status"].toString() != "SUCCESS") return;
+
+
+    QJsonArray shelvesArray = response["shelves"].toArray();
+
+    for (const QJsonValue& val : shelvesArray) {
+        QJsonObject shelfObj = val.toObject();
+
+        Shelf shelf;
+        shelf.id = shelfObj["id"].toInt();
+        shelf.name = shelfObj["name"].toString();
+
+        m_shelves.append(shelf);
+
+        QListWidgetItem* item = new QListWidgetItem(ui->shelves_listWidget);
+        item->setText(shelf.name);
+        item->setIcon(QIcon("qrc:/resources/user/close_shelf.png"));
+
+        item->setData(Qt::UserRole, shelf.id);
+
+        ui->shelves_listWidget->addItem(item);
+    }
+}
+
+
+void LibraryWidget::handleGetShelfBooks(const QJsonObject& response){
+    ui->shelfBooks_listWidget->clear();
+
+    if (response["status"].toString() != "SUCCESS") return;
+
+    QJsonArray booksArray = response["books"].toArray();
+    for (const QJsonValue& val : booksArray) {
+        QJsonObject bookObj = val.toObject();
+
+        Book book(
+            bookObj["id"].toInt(),
+            bookObj["title"].toString(),
+            bookObj["author"].toString(),
+            "", // ناشر
+            stringToGenre(bookObj["genre"].toString()),
+            bookObj["coverImagePath"].toString(),
+            0.0,
+            0.0);
+
+        BookCard* card = new BookCard(book, this);
+
+        connect(card, &BookCard::clicked, this, &LibraryWidget::on_bookCard_clicked);
+
+        QListWidgetItem* item = new QListWidgetItem(ui->shelfBooks_listWidget);
+        item->setSizeHint(card->sizeHint());
+        ui->shelfBooks_listWidget->setItemWidget(item, card);
+    }
+}
+
+void LibraryWidget::on_shelf_clicked(QListWidgetItem* item){
+    if(!item) return;
+
+    for (int i = 0; i < ui->shelves_listWidget->count() ; i++){
+        ui->shelves_listWidget->item(i)->setIcon(QIcon("qrc:/resources/user/close_shelf.png"));
+    }
+
+    item->setIcon(QIcon("qrc:/resources/user/open_shelf.png"));
+    int selectedShelfID = item->data(Qt::UserRole).toInt();
+
+    m_active_shelfID = selectedShelfID;
+
+    requestShelfBooks(selectedShelfID);
+}
+
+//هندل کردن کلیک روی کتاب های داخل کتاب های من و کتاب های قفسه ها
+void LibraryWidget::on_bookCard_clicked(Book* bookPtr){
+    if(!bookPtr) return;
+
+    //handle shelves
+
+    QMenu* menu = new QMenu(this);
+
+    menu->setStyleSheet(
+        "QMenu {"
+        "   background-color: #F4EBE1;"
+        "   border: 1px solid #D4B28C;"
+        "   border-radius: 8px;"
+        "   padding: 5px 0px;"
+        "}"
+        "QMenu::item {"
+        "   background-color: transparent;"
+        "   color: #4A3525;"
+        "   padding: 8px 25px 8px 20px;"
+        "}"
+        "QMenu::item:selected {"
+        "   background-color: #4A3525;"
+        "   color: #F7DC6F;"
+        "}"
+        "QMenu::separator {"
+        "   height: 1px;"
+        "   background-color: #D4B28C;"
+        "   margin: 4px 10px;"
+        "}"
+        );
+
+    menu->addAction("مطالعه کتاب", [this, bookPtr](){
+        //open book
+    });
+
+    int currentIndex = ui->tabWidget->currentIndex();
+
+    bool isMyBooksTab = (currentIndex == 0);
+    bool isShelvesTab = (currentIndex == 2);
+
+    if(isMyBooksTab || isShelvesTab){
+        QString menuTitle = isMyBooksTab ? "افزودن به قفسه" : "انتقال به قفسه";
+
+        QMenu* shelvesSubMenu = menu->addMenu(menuTitle);
+
+        for(const Shelf& shelf : m_shelves){
+            if(isShelvesTab && m_active_shelfID == shelf.id){
+                continue;
+            }
+
+            shelvesSubMenu->addAction(shelf.name, [this, bookPtr, shelf, isMyBooksTab](){
+                if(isMyBooksTab){
+                    //ارسال درخواست اضافه کردن کتاب به قفسه
+                }
+                else{
+                    //درخواست انتقال کتاب از مبدا به مقصد
+                }
+            });
+        }
+    }
+    menu->exec(QCursor::pos());
+
+    menu->deleteLater();
+}
+
 
 
--- Client/user/librarywidget.h
+++ Client/user/librarywidget.h
@@ -2,6 +2,15 @@
 #define LIBRARYWIDGET_H
 
 #include <QWidget>
+#include <QListWidget>
+#include "book.h"
+
+
+struct Shelf{
+    int id;
+    QString name;
+};
+
 
 namespace Ui {
 class LibraryWidget;
@@ -23,17 +32,25 @@ private slots:
 
     void processNetworkData(const QString& action, const QJsonObject& data);
 
+    void on_bookCard_clicked(Book* bookPtr);
+
+    void on_shelf_clicked(QListWidgetItem* item);
+
 private:
     Ui::LibraryWidget *ui;
     int m_userID;
+    QList <Shelf> m_shelves;
+    int m_active_shelfID;
 
     void requestPurchasedBooks();
     void requestSavedBooks();
     void requestShelves();
+    void requestShelfBooks(int shelfID);
 
     void handleGetPurchasedBooks(const QJsonObject& response);
     void handleGetSavedBooks(const QJsonObject& response);
-    // void handleGetShelves();
+    void handleGetShelves(const QJsonObject& response);
+    void handleGetShelfBooks(const QJsonObject& response);
 };
 
 #endif // LIBRARYWIDGET_H
